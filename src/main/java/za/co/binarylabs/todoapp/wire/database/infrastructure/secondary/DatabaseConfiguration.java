87088be403ea476a26f2fab6dabf2f8739625e6d
khaya <khayeni@gmail.com>
2025-06-10 11:10:56 +0200

    Apply module: jpa-postgresql

---
+++ src/main/java/za/co/binarylabs/todoapp/wire/database/infrastructure/secondary/DatabaseConfiguration.java
@@ -0,0 +1,10 @@
+package za.co.binarylabs.todoapp.wire.database.infrastructure.secondary;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = { "za.co.binarylabs.todoapp" }, enableDefaultTransactions = false)
+class DatabaseConfiguration {}
