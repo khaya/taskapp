13d2a8564863fcb86222567b96ce93636b568409
khaya <khayeni@gmail.com>
2025-06-11 18:20:24 +0200

    Setup db seeding for testing and prototyping

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO users (id, username) VALUES ('a8c06e32-2301-4445-8112-5885d0a3fb5f','alice@example.com');
+INSERT INTO users (id, username) VALUES ('87536fef-3083-4a47-9940-16937af9d8b4',  'bob@example.com');
