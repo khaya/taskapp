967797747ac10ee58bb163ab4051df500bfa1f02
khaya <khayeni@gmail.com>
2025-06-11 18:19:59 +0200

    Hard coding user-id for testing

--- src/main/java/za/co/binarylabs/taskapp/task/application/TaskApplicationService.java
+++ src/main/java/za/co/binarylabs/taskapp/task/application/TaskApplicationService.java
@@ -1,12 +1,15 @@
 package za.co.binarylabs.taskapp.task.application;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import za.co.binarylabs.taskapp.shared.error.domain.Assert;
 import za.co.binarylabs.taskapp.task.domain.*;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
+@Transactional
 public class TaskApplicationService {
 
   private final TaskRepository taskRepository;
@@ -15,9 +18,15 @@ public class TaskApplicationService {
     this.taskRepository = taskRepository;
   }
 
+
+  private User userFixture(){
+    return new User(new UserId(UUID.fromString("a8c06e32-2301-4445-8112-5885d0a3fb5f")),"alice@example.com");
+  }
+
   public Task createTask(TaskToCreate taskToCreate) {
     Assert.notNull("taskToCreate", taskToCreate);
     var task = taskToCreate.create();
+    task.setUser(userFixture());
     taskRepository.save(task);
     return task;
   }
