2e0f3d61e77708423522bd7cc0459f6307279e1c
khaya <khayeni@gmail.com>
2025-06-11 04:52:45 +0200

    Addition of userid and username validation

--- src/main/java/za/co/binarylabs/taskapp/task/domain/User.java
+++ src/main/java/za/co/binarylabs/taskapp/task/domain/User.java
@@ -1,11 +1,15 @@
 package za.co.binarylabs.taskapp.task.domain;
 
+import za.co.binarylabs.taskapp.shared.error.domain.Assert;
+
 public class User {
 
   private final UserId userId;
   private final String username;
 
   public User(UserId userId, String username) {
+    Assert.notNull("userId", userId);
+    Assert.field("username", username).notBlank();
     this.userId = userId;
     this.username = username;
   }
